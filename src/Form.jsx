d97025b97cd2ad1ae9895e2fd4d0a38cdcb82570
tom mckenna <thomasmckenna12@gmail.com>
2026-01-14 20:01:17 -0600

    minor change to styling for form

--- src/Form.jsx
+++ src/Form.jsx
@@ -1,7 +1,5 @@
 import {useState} from 'react'
 
-
-
 const Form = () => {
     const [ username, setUsername ] = useState('')
     const [ email, setEmail ] = useState('')
@@ -9,42 +7,61 @@ const Form = () => {
 
 const signUp = async (event) => {
     event.preventDefault() 
-    console.log(username, email, password)
+
+const signUpUrl = 'http://localhost:8000/signup'
+
+    try {
+        const response = await fetch(signUpUrl, {
+            method: 'POST',
+            headers: {'Content-Type': 'application/json'}, 
+            body: JSON.stringify({username, email, password})
+        })
+    if(!response.ok) {
+        console.error('there was no response')
+    }
+        const result = await response.json()
+        console.log(result)
+    } catch(err) {
+        console.log('Error Fetching', err)
+    }
 }
 
 
     return (
         <div>
             <h1>Form Page</h1>
-            <form className='form-container' onSubmit={signUp} >
-                <label htmlFor="username">
-                Username:
-                    <input 
-                        onChange={(e) => setUsername(e.target.value)}
-                        type='text'
-                        id='username'
-                    />
-                 </label>
+            <div className='form-container'>
+                <form className='form' onSubmit={signUp} >
 
-                <label htmlFor="email">
-                Email: 
-                    <input  
-                        onChange={(e) => setEmail(e.target.value)}
-                        type='email'
-                        id='email'
-                    />
-                </label>
+                    <label htmlFor="username">
+                    Username:
+                        <input 
+                            onChange={(e) => setUsername(e.target.value)}
+                            type='text'
+                            id='username'
+                            />
+                     </label>
 
-                <label htmlFor="password">
-                Passowrd: 
-                    <input  
-                        onChange={(e) => setPassword(e.target.value)}
-                        type='password'
-                        id='password'
-                    />
-                </label>
-                <button className='submit-bttn' type='submit'  >Submit form</button>
+                    <label htmlFor="email">
+                    Email: 
+                        <input  
+                            onChange={(e) => setEmail(e.target.value)}
+                            type='email'
+                            id='email'
+                            />
+                    </label>
+
+                    <label htmlFor="password">
+                    Passowrd: 
+                        <input  
+                            onChange={(e) => setPassword(e.target.value)}
+                            type='password'
+                            id='password'
+                            />
+                    </label>
+                    <button className='submit-bttn' type='submit'  >Submit form</button>
             </form>
+            </div>
         </div>
     )
 }
